761e9e1af61f18358b76c94279508303bc1952d8
Changkai <changkai.wang@outlook.com>
2023-10-17 21:35:51 +0200

    init du master1 STL s1 projet ocaml

---
+++ main.ml
@@ -0,0 +1,18 @@
+(* importation *)
+open Int64;;
+
+(* Question 1.1 
+  - entier de grand precision
+  - definir des listes d'entiers 64 bits
+  - insertion en fin de liste    
+  - preparer structure de donnees 
+  - primitive insertion
+  - primitive recuperation
+  - primitive suppression (tete de liste)
+  2^100 depasse 64bits
+  100 mod 64 = 36 (reste)
+  donc la liste [0;2^36]
+
+  si 2^164 depasse 64bits
+  164
+*)
