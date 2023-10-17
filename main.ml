49517dfde6def35ab7604b7ca00a00d4f5f09130
Changkai <changkai.wang@outlook.com>
2023-10-17 22:04:39 +0200

    ajout primitive insertion

--- main.ml
+++ main.ml
@@ -14,5 +14,20 @@ open Int64;;
   donc la liste [0;2^36]
 
   si 2^164 depasse 64bits
-  164
+  164/64 ~ 2
+  donc 164 - 64 - 64 = 36
+  on a [0;0;36]
 *)
+
+let calcul_nb_soustraction n = n-64;;
+
+let rec prim_insertion_aux n l =
+  if n>64 then 
+    let minus = n - 64 in 
+    prim_insertion_aux minus (l @ [0])
+else 
+  l @ [n];;
+
+(* n est la puissance de 2 : ex, 2^100 -> n=100 *)
+let prim_insertion n =  
+  prim_insertion_aux n [];;
