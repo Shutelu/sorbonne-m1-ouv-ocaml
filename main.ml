0bbd526b00a277cf70323219ba3b1b5af954a11a
CaoLanChuang <caolanchuang@gmail.com>
2023-11-01 18:46:11 +0100

    Realisation de DOT avec listDejaVu

--- main.ml
+++ main.ml
@@ -275,7 +275,7 @@ let rec liste_feuilles arbre =
 ;;
 
 (* 3.10 *)
-type listDejaVue = (int64 list * arbre ref) list
+type listDejaVue = (int64 list * arbre) list
 
 
 
@@ -473,3 +473,61 @@ let print_listDejaVu l =
   print_newline ();;
 
   print_listDejaVu ldv;;
+
+
+
+let to_dot_ldv ldv =
+  let buffer = Buffer.create 1024 in
+  let node_id = ref 0 in
+  let node_map = Hashtbl.create 100 in
+
+  let get_id a = 
+    try 
+      Some (Hashtbl.find node_map a)
+    with Not_found -> None
+  in
+
+  let add_id a id = 
+    Hashtbl.add node_map a id 
+  in
+
+  let rec helper (lst, a) =
+    match get_id a with
+    | Some id -> id  (* Return the existing id *)
+    | None -> (
+      match a with
+      | Feuille(b) -> 
+        let id = !node_id in
+        incr node_id;
+        add_id a id;
+        Buffer.add_string buffer (Printf.sprintf "%d [label=\"%b\"];\n" id !b);
+        id
+      | Noeud(l, v, r) ->
+        let id = !node_id in
+        incr node_id;
+        add_id a id;
+        Buffer.add_string buffer (Printf.sprintf "%d [label=\"%d\"];\n" id v);
+        let left_id = helper (lst, !l) in
+        let right_id = helper (lst, !r) in
+        Buffer.add_string buffer (Printf.sprintf "%d -- %d [style=dotted];\n" id left_id);
+        Buffer.add_string buffer (Printf.sprintf "%d -- %d [style=solid];\n" id right_id);
+        id
+    )
+  in
+
+  Buffer.add_string buffer "graph G {\n";
+  List.iter (fun tuple -> ignore (helper tuple)) ldv;
+  Buffer.add_string buffer "}\n";
+  Buffer.contents buffer
+  
+
+let save_to_dot_file_ldv filename ldv =
+  let dot_content = to_dot_ldv ldv in
+  let oc = open_out filename in
+  output_string oc dot_content;
+  close_out oc
+  
+  
+  
+let my_ldv = ldv;;
+save_to_dot_file_ldv "my_ldv.dot" my_ldv;;
